574f7fa3a12fab129012ac36d3f70dc72a3c9864
axzxs2001 <axzxs2001@163.com>
2017-09-23 10:12:02 +0800

    * .gitignore: add macdemo
    * MacDemo1.sln:
    * Program.cs:
    * MacDemo1.csproj:

---
+++ MacDemo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MacDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
